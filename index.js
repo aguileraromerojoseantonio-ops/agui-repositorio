2fb50f3c57d296cbb0e761d94c4568c7e3fff186
aguileraromerojoseantonio-ops <aguileraromerojoseantonio@gmail.com>
2026-07-13 18:16:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-// Cargar el TOKEN desde Railway
 const token = process.env.TOKEN;
 
-// Crear el bot con polling
 const bot = new TelegramBot(token, { polling: true });
 
-// Mensaje de arranque
-console.log("Bot iniciado correctamente.");
-
-// Responder a cualquier mensaje
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, "¡Hola! Tu bot está funcionando en Railway 😎");
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, '¡Hola! Tu bot está funcionando en Railway 🚀');
 });
